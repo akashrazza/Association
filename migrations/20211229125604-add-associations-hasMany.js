c4648fda6781c63ed0d722f73e48f1baf9e7e40b
Raja Kumar <rajakumar@MTXs-MacBook-Pro.local>
2021-12-30 11:32:25 +0530

    changrs

--- migrations/20211229125604-add-associations-hasMany.js
+++ migrations/20211229125604-add-associations-hasMany.js
@@ -14,8 +14,22 @@ module.exports = {
         onUpdate: 'CASCADE',
         onDelete: 'SET NULL',
       }
+    ).then(
+      queryInterface.addColumn(
+        'Credentials', // name of Source model
+        'UserDetailId', // name of the key we're adding 
+        {
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'UserDetails', // name of Target model
+            key: 'id', // key in Target model that we're referencing
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL',
+          unique:true
+        }
+      )
     )
-   
   },
 
   down: async (queryInterface, Sequelize) => {
